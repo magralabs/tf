c58b512670fd33b0bf594ad1a735749d87e14b92
Marcin Białoń <marcin.bialon@gmail.com>
2011-03-22 21:52:38 +0100

    timer start

--- public/js/timer.js
+++ public/js/timer.js
@@ -4,14 +4,24 @@ function timer_alert() {
 
 var timer = function() {
 	var current = 0 ;
-	var start = 0 ;
+	var t_start = 0 ;
 	
 	this.init = function() {
-		var d = new Date() ;
-		current = d.getTime() ;
-	    $("#timer").html( timeToStr( current ) );
+		printTime() ;
+		$( "#timer_start" ).click( start ) ;
     };
-   
+    
+    var start = function() {
+	  t_start = getTime() ;
+	  refresh();
+	};
+	
+
+    var getTime = function() {
+	  	var d = new Date() ;
+		return d.getTime() ;
+    };   
+
     var timeToStr = function( time ) {
 	   var h = parseInt( time / 3600000 ) ; // 1h * 1000 = 3600s * 1000
 	   time = time - h * 3600000 ;
@@ -22,7 +32,15 @@ var timer = function() {
 	   return h + ":" + m + ":" + s + ":" + time ;
 	};
 
-    
+    var refresh = function() {
+		current = getTime() ;
+		printTime();
+	    setTimeout( refresh, Math.random( ) * 50 ) ;
+	}
+	
+	var printTime = function() {
+		$("#timer").html( timeToStr( current - t_start ) );
+	}
 	
 } ;
 
